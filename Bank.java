527a8cb513a1804e20d214ddfeebd239452d9675
Brandon Brzuszkiewicz <bbrzuszk@psd202.org>
2018-02-06 07:07:12 -0600

    Add files via upload

--- Bank.java
+++ Bank.java
@@ -4,8 +4,12 @@ public class Bank {
 
 	private ArrayList<Account> accounts;
 	
-	public Bank(){
-		accounts = new ArrayList<Account>();
+	public Bank(ArrayList<Account> a){
+		accounts = a;
+	}
+	
+	public ArrayList<Account> getAccounts(){
+		return accounts;
 	}
 	
 	public String displayAccount(int accNum){
@@ -19,14 +23,33 @@ public class Bank {
 	public String displayCustomerAccounts(String name){
 		String output = "";
 		for(Account account: accounts){
-			if(account.getOwner() == name){
+			if(name.equalsIgnoreCase(account.getOwner())){
 				output += account.displayAccount();
+				output += "\n\n";
 			}
+			
 		}
-		return "Account Holder Does Not Exist";
+		if(output.equals(""))
+			return "Account Holder Does Not Exist";
+		return output;
 	}
 	
+	public void addAccount(String name, int accNum, double bal, double interest, boolean checking){
+		if(checking){
+			accounts.add(new CheckingAccount(name, accNum, bal));
+		}
+		else{
+			accounts.add(new Account(name, accNum, bal));
+		}
+	}
 	
+	public int findAccount(int accNum){
+		for(int i = 0; i< accounts.size();i++){
+			if(accounts.get(i).getAccNum() == accNum)
+				return i;
+		}
+		return -1;
+	}
 	
 	
 }
