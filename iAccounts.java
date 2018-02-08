e4c14834d3d84acf7b59fb42f2156c82a0c9eaac
Brandon Brzuszkiewicz <bbrzuszk@psd202.org>
2018-02-08 14:23:35 -0600

    Add files via upload

--- iAccounts.java
+++ iAccounts.java
@@ -1,6 +1,25 @@
+import java.util.ArrayList;
 
 public interface iAccounts {
 	public void deposit(double amount);
 	public double withdraw(double amount);
 	public String displayAccount();
+
+}
+
+interface iCheckingAccount{
+	public void deposit(double amount);
+	public double withdraw(double amount);
+	public String displayAccount();
+	public void depositInterest();
+}
+
+interface iBank{
+	public ArrayList<Account> getAccounts();
+	public String displayAccount(int accNum);
+	public String displayAccounts(String name);
+	public void addAccount(String name, int accNum, double bal, boolean checking);
+	public int findAccount(int accNum);
+	public int findAccount(String name);
+	
 }
